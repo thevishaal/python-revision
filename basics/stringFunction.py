df4dd2e5def38111466ef01b2aade15e66c36802
thevishaal <thevishaal0@gmail.com>
2025-09-13 21:55:41 +0530

    basic string function

---
+++ basics/stringFunction.py
@@ -0,0 +1,7 @@
+n = 'this Is PytHon'
+print(n)
+print(n.upper())    #all words capital
+print(n.lower())    #all words small
+print(n.title())    #1st letter capital of every word
+print(n.capitalize())  #1st letter of sentece capital
+print(n.swapcase())   #reverse small to capital , capital to small
