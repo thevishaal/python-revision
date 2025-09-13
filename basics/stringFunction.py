01397f84d28871e31a8bc86528167835a8245598
thevishaal <thevishaal0@gmail.com>
2025-09-13 22:02:38 +0530

    index and count function

--- basics/stringFunction.py
+++ basics/stringFunction.py
@@ -5,3 +5,16 @@ print(n.lower())    #all words small
 print(n.title())    #1st letter capital of every word
 print(n.capitalize())  #1st letter of sentece capital
 print(n.swapcase())   #reverse small to capital , capital to small
+
+
+# index function 
+name = 'Raj kumar'
+print(name.index('u'))
+print(name.find('m'))
+
+#count function
+line = 'this is my code'
+print(line.count('i'))
+
+word = 'Python code Python test'
+print(word.count('Python'))
