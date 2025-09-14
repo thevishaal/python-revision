abd78f40fe6975014cdc5310b065413fd65f13e5
thevishaal <thevishaal0@gmail.com>
2025-09-14 08:47:57 +0530

    basic function

---
+++ basics/functions.py
@@ -0,0 +1,31 @@
+#Day-02
+
+#for replace word 
+
+v = 'this is c++'
+#print(v)
+#print(v.replace('c++','python'))
+
+#split function 
+
+sp = 'It makes a list of word from string'
+#print(sp)
+#print(sp.split())
+
+#join function
+
+x = ['jan','feb','mar']
+w = ' '
+#print(w.join(x))
+
+c = 'hello'
+print(c)
+print(c.center(30,'*'))
+
+ma = 'print the maximum leter from string'
+print(max(ma))
+print(min(ma))
+
+name = 'vishal'
+r = 'hello %s'%(name)
+print(r)
