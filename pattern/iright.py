b266d5584d9ec7d2e4efe27bee745129d512536c
thevishaal <thevishaal0@gmail.com>
2025-12-09 09:05:28 +0530

    some pattern practice

---
+++ pattern/iright.py
@@ -0,0 +1,26 @@
+#inverted right angle triangle
+# lineCount = int(input("Enter Number for lines:"))
+
+# for i in range(lineCount, 0, -1):
+#     print('* '* i)
+
+# pyramid pattern 
+
+# for i in range(1, 11):
+#     print('* ' * i)
+
+# inverted pyramid pattern 
+
+# for i in range(6, 0, -1):
+#     print("* " * i)
+
+# Diamond pattern 
+
+for i in range(1,7):
+    print("* " * i)
+
+for j in range(5, 0, -1):
+    print("* "* j)
+    
+
+        
